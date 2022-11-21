03f183f890ab893014eff0c4bdb61af8db6341fb
Fikralaksana <gfikra@gmail.com>
2022-11-21 15:25:10 +0700

    adding browser

--- src/component/browser.tsx
+++ src/component/browser.tsx
@@ -1,4 +1,4 @@
-export const Console=(props:{output:string})=>{
+export const Browser=(props:{output:string})=>{
     let output=props.output.replaceAll("\r\n","<br>")
     const gethtml=()=>{
         return {__html:output}
@@ -6,6 +6,7 @@ export const Console=(props:{output:string})=>{
     return (
         <div className="bg-slate-800 h-screen w-1/3 p-2">
             <div className="h-1/2 bg-slate-900 text-white text-left p-2">
+                Ini browser
                 <span className=" font-mono" dangerouslySetInnerHTML={gethtml()}>
                 </span>
 
--- src/page/runner.tsx
+++ src/page/runner.tsx
@@ -3,8 +3,12 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { Instruction } from "../component/instruction"
 import { Writer } from "../component/writer"
-import { Output } from "../component/console"
+import { Console } from "../component/console"
+import { Browser } from "../component/browser"
 import config  from "../config.json"
+
+const using_console=['python']
+const using_browser=['web frontend']
 
 export const RunnerPage=()=>{
     const {lessons}=useParams()
@@ -18,20 +22,20 @@ export const RunnerPage=()=>{
         if(lessons){
             axios.get(crudURL).then((result:any)=>{
                 setData(result.data)
+                console.log(result.data)
             })
             axios.get(crudCodeURL,{ withCredentials: true }).then((result:any)=>{
                 setCode(result.data)
             })
         }
     },[lessons,crudCodeURL,crudURL])
-
-
     
     return (
     <div className="flex">
         <Instruction html={data.instructions}/>
         <Writer code={code} setOutput={setOutput}/>
-        <Output output={output}/>
+        {using_console.includes(data?.course?.language)&&<Console output={output}/>}
+        {using_browser.includes(data?.course?.language)&&<Browser output={output}/>}
 
     </div>)
 }
